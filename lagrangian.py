053a8bf3b845fdb1057c04093024aca29a68e257
Silvio Lacerda <silviolacerda21@hotmail.com>
2026-01-23 21:04:20 -0300

    contour plot

--- lagrangian.py
+++ lagrangian.py
@@ -1,38 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import NearestNDInterpolator
+from scipy.ndimage import gaussian_filter
 
+
+############################################################
+                # Parâmetros
+############################################################
 
 c0 = 1500
 b = 2
+z0 = 10**3
+x0 = 0
 za = 1300
 eps = 0.00737
 dl = 10
 L = 50*(10**3)
 maxz = 5*(10**3)
 zz = np.arange(0, maxz, 0.1)
-
-
+dtheta = 0.005
+thetaInterval = np.arange(-0.1, 0.1, dtheta)
 N = int(L / dl)
+
+############################################################
+                # Funções 
+############################################################
+
 
 def c(z):
     zbar = b * (z - za) / za
     return c0 * (1.0 + eps*(zbar - 1.0 + np.exp(-zbar)))
 
-def dcdz(z):
+def dc(z):
     zbar = b * (z - za) / za
     return (c0*eps*b/za)*(1.0 - np.exp(-zbar))
 
-def rk_step(x, z, xi, eta, dl):
+def rk2(x, z, xi, eta, dl):
     cc = c(z)
-    dc = dcdz(z)
+    dcc = dc(z)
 
     x1 = x + dl*cc*xi
     z1 = z + dl*cc*eta
     xi1 = xi
-    eta1 = eta - dl*dc/(cc**2)
+    eta1 = eta - dl*dcc/(cc**2)
 
     c2 = c(z1)
-    dc2 = dcdz(z1)
+    dc2 = dc(z1)
 
     x2 = x + dl * c2 * xi
     z2 = z + dl * c2 * eta
@@ -41,81 +54,115 @@ def rk_step(x, z, xi, eta, dl):
 
     return (x1 + x2)/2, (z1 + z2)/2, (xi1 + xi2)/2, (eta1 + eta2)/2
 
-plt.figure(figsize=(8,5))
 
-dtheta = 0.005
-angInterval = np.arange(-0.1, 0.1001, dtheta)
-cc = c(zz)
+def createRays(angles):
+    rays = []
+    for ang in angles:
+        x = np.array([])
+        z = np.array([])
+        xi = np.array([])
+        eta = np.array([])
+        S = np.array([])
 
-rays = []
+        x = np.append(x, x0)
+        z = np.append(z, z0)
+        S = np.append(S, 0)
 
-for ang in angInterval:
+        c1 = c(z[0])
+        
+        xi = np.append(xi, np.cos(ang)/c1)
+        eta = np.append(eta, np.sin(ang)/c1)
 
-    x = np.zeros(N + 1)
-    z = np.zeros(N + 1)
-    xi = np.zeros(N + 1)
-    eta = np.zeros(N + 1)
-    S = np.zeros(N + 1)
+        for i in range(N):
+            xn, zn, xin, etan = rk2(x[i], z[i], xi[i], eta[i], dl)
+            Sn = S[i] + dl / c(z[i])
+            x = np.append(x, xn)
+            z = np.append(z, zn)
+            xi = np.append(xi, xin)
+            eta = np.append(eta, etan)
+            S = np.append(S, Sn)
 
-    x[0] = 0.0
-    z[0] = 1000.0
+        rays.append((x, z))
+        
+    return rays
 
-    c1 = c(z[0])
+def plotRays(rays):
+    plt.figure(figsize=(8,5))
+    for ray in rays:
+        plt.plot(ray["x"], ray["z"])
 
-    xi[0] = np.cos(ang) / c1
-    eta[0] = np.sin(ang) / c1
-
-    for i in range(N):
-        x[i+1], z[i+1], xi[i+1], eta[i+1] = rk_step(x[i], z[i], xi[i], eta[i], dl)
-        S[i+1] = S[i] + dl / c(z[i])
-
-    rays.append({"x": x, "z": z, "S": S})
-
-for ray in rays:
-    plt.plot(ray["x"], ray["z"])
-
-plt.gca().invert_yaxis()
-plt.xlabel("x (m)")
-plt.ylabel("z (m)")
-plt.title("Ray tracing")
-plt.grid(True)
-plt.show()
-
-
-# A0 = []
-
-# for k in range(1, len(rays)-1):
-#     xm = rays[k-1]["x"]
-#     zm = rays[k-1]["z"]
-#     xp = rays[k+1]["x"]
-#     zp = rays[k+1]["z"]
-
-#     x0 = rays[k]["x"]
-#     z0 = rays[k]["z"]
-
-#     dxdth = (xp - xm) / (2*dtheta)
-#     dzdth = (zp - zm) / (2*dtheta)
-
-#     J = np.sqrt(dxdth**2 + dzdth**2)
-
-#     # estou usando que l_0 = 1, A_0(l_0) = 1
-
-#     A = np.sqrt(c(z0) / c(z0[0])) / np.sqrt(J)
-#     A0.append(A)
+    plt.gca().invert_yaxis()
+    plt.xlabel("x (m)")
+    plt.ylabel("z (m)")
+    plt.title("Ray tracing")
+    plt.grid(True)
+    plt.show()
 
 
+def evaluateA0(rays):
+    A0 = []
 
-# mid = len(A0)//2
+    for k in range(1, len(rays) - 1):
+        xm, zm = rays[k-1]
+        xp, zp = rays[k+1]
+        xk, zk = rays[k]
 
-# # 1j é como o python entende a unidade imaginária
-# u = A0[mid] * np.exp(1j * rays[mid+1]["S"])
+        dx_dth = (xp - xm) / (2 * dtheta)
+        dz_dth = (zp - zm) / (2 * dtheta)
 
-# plt.figure()
+        J = np.sqrt(dx_dth**2 + dz_dth**2)
+        J[J < 1e-6] = 1e-6  # regularização
 
-# plt.plot(rays[mid+1]["x"], np.abs(u))
-# plt.xlabel("x (m)")
-# plt.ylabel("|u|")
-# plt.title("Amplitude |u| ao longo do raio central")
-# plt.grid(True)
+        A = np.sqrt(c(zk) / c(zk[0])) / np.sqrt(J)
+        A0.append(A)
 
-# plt.show()
+    return A0
+
+def evaluateSolution(A0):
+    nx, nz = 500, 300
+    xg = np.linspace(0, 50_000, nx)
+    zg = np.linspace(500, 2500, nz)
+
+    dx = xg[1] - xg[0]
+    dz = zg[1] - zg[0]
+
+    I = np.zeros((nz, nx))
+
+    for k in range(len(A0)):
+        xk, zk = rays[k+1]
+        Ak = A0[k]
+
+        for i in range(len(xk)):
+            ix = int((xk[i] - xg[0]) / dx)
+            iz = int((zk[i] - zg[0]) / dz)
+
+            if 0 <= ix < nx and 0 <= iz < nz:
+                I[iz, ix] += Ak[i]**2
+
+    I = gaussian_filter(I, sigma=2)
+    return xg, zg, I
+
+def plotSolution(xg, zg, I):
+    V = 10 * np.log10(I + 1e-12)
+
+    plt.figure(figsize=(10,5))
+    levels = np.linspace(-40, 0, 60)
+
+    plt.contourf(xg, zg, V, levels=levels, cmap="jet", extend="both")
+    plt.colorbar(label="Intensity (dB)")
+    plt.gca().invert_yaxis()
+    plt.xlabel("Range (m)")
+    plt.ylabel("Depth (m)")
+    plt.title("u(x,y)²")
+    plt.show()
+
+
+############################################################
+                # Aplicando 
+############################################################
+
+rays = createRays(thetaInterval)
+# plotRays(rays)
+A0 = evaluateA0(rays)
+xg, zg, I = evaluateSolution(A0)
+plotSolution(xg, zg, I)
